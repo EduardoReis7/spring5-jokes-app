dda7d0176417113c9ddd9a906b5581bda13914ee
EduardoReis7 <eduardodovigireis@gmail.com>
2020-07-22 17:15:42 -0300

    created the joke controller

---
+++ src/main/java/com/er/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package com.er.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.er.services.JokeService;
+
+@Controller
+public class JokeController {
+
+	private JokeService jokeService;
+
+	public JokeController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/", ""})
+	public String showJoke(Model model) {
+		
+		model.addAttribute("joke", jokeService.getJoke());
+		
+		return "chucknorris";
+	}
+	
+}
